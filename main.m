1ad87eff9759f7fa4da5d23237c52af563784882
Bruno Miguel <migueldireito@gmail.com>
2019-04-26 14:39:58 +0100

    Update main.m

--- main.m
+++ main.m
@@ -5,30 +5,25 @@ clear all;                          % clear current workspace
 addpath(genpath('libs\BiomotionToolbox\BiomotionToolbox'))
 
 %%
-
-% BMT_demo_1_Basic is one of the most basic programs. It initializes a bvh
-% file (with the 'wrong' extension 'txt'), and then shows it.
-
-
-
  
-combiname = fullfile(pwd,'libs/BiomotionToolbox/BiomotionToolbox/BiomotionToolboxDemos/actions/19_01.txt');   % exact file location
+combiname = fullfile(pwd,'libs','BiomotionToolbox','BiomotionToolbox',...
+    'BiomotionToolboxDemos','actions','19_01.txt'); % exact file location
  
-win = SetScreen('OpenGL',1, 'Window', 1);   % Open a screen
-SetProjection(win);                         % Set the type of projection 
+win = SetScreen('OpenGL', 1, 'Window', 1);  % Open a screen.
+SetProjection(win);                         % Set the type of projection .
 
 try   
-    bm = BioMotion(combiname,'Filetype','bvh'); % load the data, tell the function %that you have a bvh file even though the extension is txt
-    bm.Loop = 1;                            % Set the parameter to Loop
+    bm = BioMotion(combiname,'Filetype','bvh'); % load the data, tell the function %that you have a bvh file even though the extension is txt.
+    bm.Loop = 1;                            % Set the parameter to Loop.
 
     startTime=GetSecs;
     
-    for fr = 1:bm.nFrames                   % Run through frames 1 until the end
-        if KbCheck                          % Check for button presses, exit if any
+    for fr = 1:bm.nFrames                   % Run through frames 1 until the end.
+        if KbCheck                          % Check for button presses, exit if any.
             break;
         end
-        moglDrawDots3D(win.Number,bm.GetFrame(fr),2); % draw the dots, note the call %to GetFrame 
-        Screen('Flip',win.Number);  %display the dots on the screen
+        moglDrawDots3D(win.Number,bm.GetFrame(fr),2); % draw the dots, note the call %to GetFrame.
+        Screen('Flip',win.Number);          % Display the dots on the screen.
     end
     
     endTime=GetSecs-startTime;
